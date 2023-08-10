e0d4fefbb6e923161c2af8acbfcb4aa55fc2a692
ta <tuananh31020@gmail.com>
2023-08-10 10:29:32 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function ()
+	{
+		return redirect()->route('info.get');
+	});
 // Form Info
-Route::get('/', [CustomerController::class, 'index']);
+Route::get('/ThailandMartech2023', [CustomerController::class, 'index'])->name('info.get');
 Route::post('/customer-store', [CustomerController::class, 'store'])->name('customer.store');
 
 //Auth
